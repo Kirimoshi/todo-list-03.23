542c47e9342fdcead11c85283bf3c8f3801d930e
Kirimoshi <baronaxaro@gmail.com>
2023-03-31 16:50:41 +0300

    some eslint fixes

--- src/components/pages/WelcomePage.tsx
+++ src/components/pages/WelcomePage.tsx
@@ -1,4 +1,4 @@
-import {Link, redirect} from "react-router-dom";
+import {Link} from "react-router-dom";
 // @ts-ignore
 import styles from './WelcomePage.module.css';
 import {useDispatch} from "react-redux";
